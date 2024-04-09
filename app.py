d827885a694dfee263bc4b8188d906c8145a9e80
Mingy123 <mingy123.coc@gmail.com>
2024-04-09 15:05:10 +0800

    updaet labels

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ from segment import segmenter_list_foods
 from PIL import Image
 import tensorflow as tf
 
-class_names = ['Drink_Bottle_PokkaGreenTea', 'Drink_Bottle_PokkaIcePeachTea', 'Drink_Bottle_Sencha', 'Drink_Can_100PlusActive', 'Drink_Can_100PlusOriginal', 'Drink_Can_FuzeteaJasmine', 'Drink_Can_FuzeteaLemon', 'Drink_Can_FuzeteaLychee', 'Drink_Can_Houjicha', 'Drink_Can_Sencha', 'Drink_Carton_MarigoldHLMilk', 'Drink_Carton_YeosCoconutWater']
+class_names = ['Drink_Bottle_FruitTreeApple', 'Drink_Bottle_H2ONonCarbonated', 'Drink_Bottle_H2OOriginal', 'Drink_Bottle_PinkDolphinPeach', 'Drink_Bottle_PokkaGreenTea', 'Drink_Bottle_PokkaIcePeachTea', 'Drink_Bottle_PokkaSparklinFujiApple', 'Drink_Bottle_Sencha', 'Drink_Bottle_YeosLemonBarley', 'Drink_Can_100PlusActive', 'Drink_Can_100PlusOriginal', 'Drink_Can_FuzeteaJasmine', 'Drink_Can_FuzeteaLemon', 'Drink_Can_FuzeteaLychee', 'Drink_Can_Houjicha', 'Drink_Can_Sencha', 'Drink_Carton_MarigoldHLChocolate', 'Drink_Carton_MarigoldHLStrawberry', 'Drink_Carton_NutrisoyOatsAndQuinoa', 'Drink_Carton_NutrisoyOriginal', 'Drink_Carton_NutrisoyReducedSugar', 'Drink_Carton_YeosCoconutWater']
+
 os.makedirs('/tmp/csaiproj', exist_ok=True)
 FILENAMES = string.ascii_letters + string.digits
 def random_filename(n):
